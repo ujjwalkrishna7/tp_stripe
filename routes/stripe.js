ee8c016b713f8f773c56b7c120661be7d9d681f6
Ujjwal Krishna <ujjwalkrishna7@gmail.com>
2023-03-23 11:42:17 +0530

    first commit

---
+++ routes/stripe.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const Stripe = require("stripe");
+const { Order } = require("../models/Order");
+
+require("dotenv").config();
+
+const stripe = Stripe(process.env.STRIPE_KEY);
+
+const router = express.Router();
+
+router.post("/create-checkout-session", async (req, res) => {
+  console.log(req.body);
+  const customer = await stripe.customers.create({
+    metadata: {
+      userId: req.body.userId,
+      cart: JSON.stringify(req.body.cartItems),
+    },
+  });
+
+  const line_items = req.body.items.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+
+    phone_number_collection: {
+      enabled: true,
+    },
+    line_items,
+    mode: "payment",
+    customer: customer.id,
+    success_url: `${process.env.CLIENT_URL}/PaymentSuccess`,
+    cancel_url: `${process.env.CLIENT_URL}/PaymentService`,
+  });
+
+  // res.redirect(303, session.url);
+  res.send({ url: session.url });
+});
+
+// Create order function
+
+const createOrder = async (customer, data) => {
+  const Items = JSON.parse(customer.metadata.cart);
+
+  const products = Items.map((item) => {
+    return {
+      productId: item.id,
+      quantity: item.quantity,
+    };
+  });
+
+  const newOrder = new Order({
+    userId: customer.metadata.userId,
+    customerId: data.customer,
+    paymentIntentId: data.payment_intent,
+    products,
+    subtotal: data.amount_subtotal,
+    total: data.amount_total,
+    shipping: data.customer_details,
+    payment_status: data.payment_status,
+  });
+
+  try {
+    const savedOrder = await newOrder.save();
+    console.log("Processed Order:", savedOrder);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Stripe webhoook
+
+router.post(
+  "/webhook",
+  express.json({ type: "application/json" }),
+  async (req, res) => {
+    let data;
+    let eventType;
+    console.log(req.body);
+
+    // Check if webhook signing is configured.
+    let webhookSecret;
+    webhookSecret = process.env.STRIPE_WEB_HOOK;
+
+    if (webhookSecret) {
+      // Retrieve the event by verifying the signature using the raw body and secret.
+      let event;
+      let signature = req.headers["stripe-signature"];
+
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          signature,
+          webhookSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed:  ${err}`);
+        return res.sendStatus(400);
+      }
+      // Extract the object from the event.
+      data = event.data.object;
+      eventType = event.type;
+    } else {
+      // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+      // retrieve the event data directly from the request body.
+      data = req.body.data.object;
+      eventType = req.body.type;
+    }
+
+    // Handle the checkout.session.completed event
+    if (eventType === "checkout.session.completed") {
+      stripe.customers
+        .retrieve(data.customer)
+        .then(async (customer) => {
+          try {
+            // CREATE ORDER
+            console.log("create order");
+            console.log(customer);
+            console.log(data);
+            // createOrder(customer, data);
+          } catch (err) {
+            console.log(typeof createOrder);
+            console.log(err);
+          }
+        })
+        .catch((err) => console.log(err.message));
+    }
+
+    res.status(200).end();
+  }
+);
+
+module.exports = router;
